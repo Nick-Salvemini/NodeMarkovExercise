0673348c01b1622642cd75a6fba2b62ef8bf962c
Nick Salvemini <salvemini.nick@gmail.com>
2023-08-16 15:24:13 -0400

    worked

--- markov.js
+++ markov.js
@@ -17,13 +17,44 @@ class MarkovMachine {
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
   makeChains() {
-    // TODO
+    let chains = new Map();
+
+    for (let i = 0; i < this.words.length; i++) {
+      let word = this.words[i];
+      let nextWord = this.words[i + 1] || null
+
+      if (chains.has(word)) {
+        chains.get(word).push(nextWord)
+      } else { chains.set(word, [nextWord]) }
+    }
+    this.chains = chains
+    console.log(chains)
   }
 
-
+  static choice(arr) {
+    return arr[Math.floor(Math.random() * arr.length)]
+  }
   /** return random text from chains */
 
   makeText(numWords = 100) {
-    // TODO
+    let str = ''
+    let chains = this.chains
+    let keysArr = Array.from(chains.keys())
+
+    for (let i = 0; i < numWords; i++) {
+      let word1 = keysArr[MarkovMachine.choice(keysArr)];
+      let valArr = chains.get(word1);
+      let word2 = valArr[MarkovMachine.choice(valArr)]
+
+      str += word1;
+      if (word2 != null) {
+        str += word2
+      }
+    }
+    return str
   }
 }
+
+let mm = new MarkovMachine("the cat in the hat")
+mm.makeText();
+mm.makeText(numWords = 50);
