4b4710dcbb20f81338abe8ebc2878efde467771a
Nick Salvemini <salvemini.nick@gmail.com>
2023-08-21 16:31:22 -0400

    updated

--- makeText.js
+++ makeText.js
@@ -3,24 +3,56 @@ const markov = require('./markov');
 const axios = require("axios");
 const process = require("process");
 
-function generateText(text) {
-    let mm = new markov.MarkovMachine(text);
-    console.log(mm.makeText())
+// function generateText(text) {
+//     // let mm = new markov.MarkovMachine(text);
+//     // console.log(mm.makeText())
+//     console.log(text)
+// }
+
+// function read(path) {
+//     fs.readFile(path, 'utf8', (err, data) => {
+//         if (err) {
+//             console.log('Error: ', err);
+//             process.exit(1)
+//         } else {
+//             // console.log(typeof data)
+//             return data
+//         }
+//     })
+// }
+
+async function generateText(url) {
+    try {
+        let resp = await axios.get(url);
+        console.log(resp)
+    } catch (err) {
+        console.log(`Error.  Could Not Find ${url}: ${err}`);
+        process.exit(1);
+    }
+
 }
 
-function read(path) {
-    fs.readFile(path, 'utf8', (err, data) => {
+let read = new Promise((resolve, reject) => {
+    fs.readFile(process.argv[3], 'utf8', (err, data) => {
         if (err) {
-            console.log('Error: ', err);
-            process.exit(1)
+            reject(err);
         } else {
             // console.log(typeof data)
-            return data
+            resolve(data)
         }
     })
+})
+
+if (process.argv[2] == 'file') {
+    read.then(data => console.log(data)).catch(err => { console.log('Error: ', err) })
 }
 
-generateText(read(process.argv[3]))
+if (process.argv[2] == 'url') {
+    // read.then(generateText(process.argv[3])).catch(err => { console.log('Error: ', err) })
+    generateText(process.argv[3])
+}
+
+// generateText(read(process.argv[3]))
 // let text = read(process.argv[3])
 // console.log(text)
 
